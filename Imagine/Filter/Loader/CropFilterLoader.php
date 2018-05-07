7a9667fa96e41c705d2b0aa796f364409f57fbf6
GregHubs <gregory@bird-office.com>
2018-05-07 15:25:51 +0700

    crop filter => zoom in the middle

--- Imagine/Filter/Loader/CropFilterLoader.php
+++ Imagine/Filter/Loader/CropFilterLoader.php
@@ -23,13 +23,22 @@ class CropFilterLoader implements LoaderInterface
      */
     public function load(ImageInterface $image, array $options = [])
     {
-        $x = isset($options['start'][0]) ? $options['start'][0] : null;
-        $y = isset($options['start'][1]) ? $options['start'][1] : null;
-
         $width = isset($options['size'][0]) ? $options['size'][0] : null;
         $height = isset($options['size'][1]) ? $options['size'][1] : null;
 
+        $size = $image->getSize();
+        $origWidth = $size->getWidth();
+        $origHeight = $size->getHeight();
+
+        $deltaWidth = $origWidth - $options['size'][0];
+        $x = $deltaWidth / 2;
+
+        $deltaHeight = $origHeight - $options['size'][1];
+        $y = $deltaHeight / 2;
+
+
         $filter = new Crop(new Point($x, $y), new Box($width, $height));
+
         $image = $filter->apply($image);
 
         return $image;
